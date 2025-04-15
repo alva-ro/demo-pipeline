cd9ef679a45c2723bf48f2b5399583b69fca63bf
Alvaro <alvaro.t.pardo@gmail.com>
2025-04-14 21:23:39 -0400

    correccion de llamada a aplicacion

--- pioo/tests.py
+++ pioo/tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 class PingTestCase(TestCase):
     def test_ping(self):
         fecha_esperada = "2024-03-20T12:00:00"
-        with patch("myapp.views.datetime") as mock_datetime:
+        with patch("pioo.views.datetime") as mock_datetime:
             mock_datetime.now.return_value = datetime.fromisoformat(fecha_esperada)
             response = self.client.get("/ping/")
             self.assertEqual(response.status_code, 200)
